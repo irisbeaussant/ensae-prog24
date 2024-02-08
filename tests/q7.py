96ae6092028341ce8ae094232b3ff102c461eb1f
ibeaussant-ensae <iris.beaussant@ensae.fr>
2024-02-08 18:33:56 +0000

    modifs 8/02

---
+++ tests/q7.py
@@ -0,0 +1,30 @@
+# This will work if ran from the root folder ensae-prog24
+
+
+import sys
+sys.path.append("swap_puzzle/")
+
+
+
+
+import unittest
+from graph import Graph
+from grid import Grid
+
+
+
+
+class Test_Cheminlepluscourt(unittest.TestCase):
+   def test_cheminlepluscourt(self):
+       A = Grid.grid_from_file("input/grid5.in")
+       B = [[1, 2], [3, 4]]
+       chemin = Graph.chemin_le_plus_court(Graph, A, B)
+       self.assertEqual(chemin, [A, [[1, 2], [4, 3]], B]) 
+
+
+
+
+
+
+if __name__ == '__main__':
+   unittest.main()
